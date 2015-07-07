eac9bdcc4770891caf80c82f5007ef209f8de204
allsources <belikov.andrey@hotmail.com>
2015-07-07 16:42:32 +0300

    Update WordDictionaryCollection.cs

--- WordAutoComplete/WordCollections/WordDictionaryCollection.cs
+++ WordAutoComplete/WordCollections/WordDictionaryCollection.cs
@@ -7,6 +7,7 @@ namespace WordAutoComplete.WordCollections
 {
   /// <summary>
   /// Represents words collection based on <see cref="Dictionary"/> object.
+  /// This class created to compare performance with the prefix-tree class.
   /// </summary>
   public class WordDictionaryCollection : Dictionary<string, Word>, IWordCollection
   {
